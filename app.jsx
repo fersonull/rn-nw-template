ceb92f585847aff6c2c5f5374cfda573bf856d56
fersonull <culaspero@gmail.com>
2025-12-09 19:56:56 +0800

    add safe area

--- app.jsx
+++ app.jsx
@@ -1,11 +1,22 @@
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 import { View, Text } from 'react-native'
 import React from 'react'
 import './global.css'
 
 export default function App() {
   return (
-    <View>
-      <Text>app</Text>
-    </View>
+    <SafeAreaProvider>
+      <AppContent />
+    </SafeAreaProvider>
+  )
+}
+
+function AppContent() {
+  return (
+    <SafeAreaView>
+      <View className=''>
+        <Text>Welcome!</Text>
+      </View>
+    </SafeAreaView>
   )
 }
